fcdb97d37e243370eec61051b1925ab88c3ee522
jassmanni <jassmanniq@gmail.com>
2019-07-29 16:31:40 -0400

    CRUD de usuario completado

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
+
+
 const app = express();
 const bodyParser = require('body-parser');
 
@@ -10,41 +13,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.get('/usuario', function (req, res) {
-    res.json('get usuario')
+
+//rutas
+app.use (require('./routes/usuario'));
+
+//Conexion a base de datos
+mongoose.connect('mongodb://localhost:27017/cafe', (err, res) => {
+    if(err) throw err;
+    console.log('Base de datos ONLINE');
 });
 
-app.post('/usuario', function (req, res) {
-
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-
-});
-
-app.put('/usuario/:id', function (req, res) {
-
-    let id = req.params.id;
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', function (req, res) {
-    res.json('delete usuario')
-});
-
-
-
+//Levantando el servidor
 app.listen(process.env.PORT, () => {
     console.log(`Escuchando el puerto ${process.env.PORT}`);
 })
